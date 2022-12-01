f8a388c457a947ee67db34ff33233e6add41d4c6
hiromisugie <lozz.gggg@gmail.com>
2022-12-02 04:29:23 +0900

    メモ詳細ページの更新など

--- app.rb
+++ app.rb
@@ -14,12 +14,26 @@ get '/' do
   erb :index
 end
 
+# メモ：リダイレクトは下記のように設定可能
+get '/hoge' do
+  redirect 'http://localhost:4567/detail/4933535d-8f82-4d1e-a7bb-a04409ce576f'
+end
+
 get '/new' do
   @title = "新規作成 | メモアプリ"
   erb :new
 end
 
-get '/detail/' do
+get '/detail/:id' do
+  @path = params['id']
+  @detail_hash = File.open("json/db.json") { |f| JSON.load(f) } # JSONファイル内のJSONオブジェクトをRubyオブジェクトに変換して@detail_hashに入れる
+  @detail_memos = @detail_hash["memos"]
+
+  @title = "メモ | メモアプリ"
+  erb :detail
+end
+
+get '/detail' do
   @title = "メモ | メモアプリ"
   erb :detail
 end
@@ -33,8 +47,7 @@ post '/detail' do
   hash["memos"] << {"id" => @id, "title" => @memo_title, "body" => @memo_text} # JSONの方で設定していたmemosという配列に要素を追加
   File.open("json/db.json", "w") { |f| JSON.dump(hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
 
-  @title = "#{@memo_title} | メモアプリ" #HTMLのTitleタグ
-  erb :detail
+  redirect "/detail/#{@id}"
 end
 
 get '/edit' do
