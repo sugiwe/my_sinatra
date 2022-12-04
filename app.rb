9a0191ecd66f6dcd1f1e5b61c939d3e34ea9e38f
hiromisugie <lozz.gggg@gmail.com>
2022-12-05 05:11:38 +0900

    メモの編集、削除機能などの追加

--- app.rb
+++ app.rb
@@ -3,8 +3,14 @@ require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
 
+helpers do
+  def open_json(file)
+    File.open(file) { |f| JSON.load(f) }
+  end
+end
+
 get '/' do
-  @hash = File.open("json/db.json") { |f| JSON.load(f) } # JSONファイル内のJSONオブジェクトをRubyオブジェクトに変換してhashという変数に入れる
+  @hash = open_json("json/db.json") 
   @memos = @hash["memos"]
   @memo_titles = @memos.map{|memo| memo["title"]}
   @memo_ids = @memos.map{|memo| memo["id"]}
@@ -14,11 +20,6 @@ get '/' do
   erb :index
 end
 
-# メモ：リダイレクトは下記のように設定可能
-get '/hoge' do
-  redirect 'http://localhost:4567/detail/4933535d-8f82-4d1e-a7bb-a04409ce576f'
-end
-
 get '/new' do
   @title = "新規作成 | メモアプリ"
   erb :new
@@ -26,7 +27,7 @@ end
 
 get '/detail/:id' do
   @path = params['id']
-  @detail_hash = File.open("json/db.json") { |f| JSON.load(f) } # JSONファイル内のJSONオブジェクトをRubyオブジェクトに変換して@detail_hashに入れる
+  @detail_hash = open_json("json/db.json")
   @detail_memos = @detail_hash["memos"]
 
   @title = "メモ | メモアプリ"
@@ -43,14 +44,61 @@ post '/detail' do
   @memo_text = params[:memo_text]
   @id = SecureRandom.uuid # メモにユニークなIDを付ける
 
-  hash = File.open("json/db.json") { |f| JSON.load(f) } # JSONファイル内のJSONオブジェクトをRubyオブジェクトに変換してhashという変数に入れる
+  hash = open_json("json/db.json")
   hash["memos"] << {"id" => @id, "title" => @memo_title, "body" => @memo_text} # JSONの方で設定していたmemosという配列に要素を追加
   File.open("json/db.json", "w") { |f| JSON.dump(hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
 
   redirect "/detail/#{@id}"
 end
 
+get '/edit/:id' do
+  @path = params['id']
+  @detail_hash = open_json("json/db.json")
+  @detail_memos = @detail_hash["memos"]
+
+  @title = "メモの編集 | メモアプリ"
+  erb :edit
+end
+
 get '/edit' do
   @title = "メモの編集 | メモアプリ"
   erb :edit
+end
+
+post '/edit/:id' do
+  @path = params['id']
+  @memo_title = params[:memo_title]
+  @memo_text = params[:memo_text]
+
+  hash = open_json("json/db.json")
+  memos = hash["memos"]
+  memos.map do |memo|
+    if memo.has_value?(@path) 
+      memo.store("title", @memo_title)
+      memo.store("body", @memo_text)
+    end
+  end
+  File.open("json/db.json", "w") { |f| JSON.dump(hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
+
+  redirect "/detail/#{@path}"
+end
+
+get '/delete/:id' do
+  @path = params['id']
+  @detail_hash = open_json("json/db.json")
+  @detail_memos = @detail_hash["memos"]
+
+  @title = "メモの削除 | メモアプリ"
+  @content = "このメモを削除しますか？"
+  erb :delete
+end
+
+delete '/delete/:id' do
+  @path = params['id']
+  @detail_hash = open_json("json/db.json")
+  @detail_memos = @detail_hash["memos"]
+  @detail_memos.delete_if{|memo| memo.has_value?(@path)} # 該当するメモを削除する
+  File.open("json/db.json", "w") { |f| JSON.dump(@detail_hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
+
+  redirect "/"
 end
