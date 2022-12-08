2b232dac62091aa7b031d06af99a4f73aac680c4
hiromisugie <lozz.gggg@gmail.com>
2022-12-09 04:31:38 +0900

    主にrubocopの指摘を修正

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-gem "sinatra" # 追記
+gem 'sinatra' # 追記
 gem 'sinatra-contrib' # 追記
-gem "webrick" # 追記
+gem 'webrick' # 追記
--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
@@ -5,63 +7,66 @@ require 'securerandom'
 
 helpers do
   def open_json(file)
-    File.open(file) { |f| JSON.load(f) }
+    File.open(file) { |f| JSON.parse(f.read) }
+  end
+
+  def overwrite_json(file, hash)
+    File.open(file, 'w') { |f| JSON.dump(hash, f) }
   end
 end
 
 get '/' do
-  @hash = open_json("json/db.json") 
-  @memos = @hash["memos"]
-  @memo_titles = @memos.map{|memo| memo["title"]}
-  @memo_ids = @memos.map{|memo| memo["id"]}
+  @hash = open_json('json/db.json')
+  @memos = @hash['memos']
+  @memo_titles = @memos.map { |memo| memo['title'] }
+  @memo_ids = @memos.map { |memo| memo['id'] }
 
-  @title = "メモアプリ"
-  @content = "たくさんメモしよう！"
+  @title = 'メモアプリ'
   erb :index
 end
 
 get '/new' do
-  @title = "新規作成 | メモアプリ"
+  @title = '新規作成 | メモアプリ'
   erb :new
 end
 
 get '/detail/:id' do
   @path = params['id']
-  @detail_hash = open_json("json/db.json")
-  @detail_memos = @detail_hash["memos"]
+  @detail_hash = open_json('json/db.json')
+  @detail_memos = @detail_hash['memos']
 
-  @title = "メモ | メモアプリ"
+  @title = 'メモ | メモアプリ'
   erb :detail
 end
 
 get '/detail' do
-  @title = "メモ | メモアプリ"
+  @title = 'メモ | メモアプリ'
   erb :detail
 end
 
 post '/detail' do
   @memo_title = params[:memo_title]
   @memo_text = params[:memo_text]
-  @id = SecureRandom.uuid # メモにユニークなIDを付ける
+  @id = SecureRandom.uuid
 
-  hash = open_json("json/db.json")
-  hash["memos"] << {"id" => @id, "title" => @memo_title, "body" => @memo_text} # JSONの方で設定していたmemosという配列に要素を追加
-  File.open("json/db.json", "w") { |f| JSON.dump(hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
+  hash = open_json('json/db.json')
+  hash['memos'] << { 'id' => @id, 'title' => @memo_title, 'body' => @memo_text }
+  overwrite_json('json/db.json', hash)
 
   redirect "/detail/#{@id}"
 end
 
 get '/edit/:id' do
   @path = params['id']
-  @detail_hash = open_json("json/db.json")
-  @detail_memos = @detail_hash["memos"]
+  @detail_hash = open_json('json/db.json')
+  @detail_memos = @detail_hash['memos']
 
-  @title = "メモの編集 | メモアプリ"
+  @title = 'メモの編集 | メモアプリ'
   erb :edit
 end
 
 get '/edit' do
-  @title = "メモの編集 | メモアプリ"
+  @title = 'メモの編集 | メモアプリ'
   erb :edit
 end
 
@@ -70,35 +75,35 @@ post '/edit/:id' do
   @memo_title = params[:memo_title]
   @memo_text = params[:memo_text]
 
-  hash = open_json("json/db.json")
-  memos = hash["memos"]
+  hash = open_json('json/db.json')
+  memos = hash['memos']
   memos.map do |memo|
-    if memo.has_value?(@path) 
-      memo.store("title", @memo_title)
-      memo.store("body", @memo_text)
+    if memo.value?(@path)
+      memo.store('title', @memo_title)
+      memo.store('body', @memo_text)
     end
   end
-  File.open("json/db.json", "w") { |f| JSON.dump(hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
+  overwrite_json('json/db.json', hash)
 
   redirect "/detail/#{@path}"
 end
 
 get '/delete/:id' do
   @path = params['id']
-  @detail_hash = open_json("json/db.json")
-  @detail_memos = @detail_hash["memos"]
+  @detail_hash = open_json('json/db.json')
+  @detail_memos = @detail_hash['memos']
 
-  @title = "メモの削除 | メモアプリ"
-  @content = "このメモを削除しますか？"
+  @title = 'メモの削除 | メモアプリ'
+  @content = 'このメモを削除しますか？'
   erb :delete
 end
 
 delete '/delete/:id' do
   @path = params['id']
-  @detail_hash = open_json("json/db.json")
-  @detail_memos = @detail_hash["memos"]
-  @detail_memos.delete_if{|memo| memo.has_value?(@path)} # 該当するメモを削除する
-  File.open("json/db.json", "w") { |f| JSON.dump(@detail_hash, f) } # 更新したhashをJSONオブジェクトに変換してJSONファイルに上書きする
+  @detail_hash = open_json('json/db.json')
+  @detail_memos = @detail_hash['memos']
+  @detail_memos.delete_if { |memo| memo.value?(@path) }
+  overwrite_json('json/db.json', @detail_hash)
 
-  redirect "/"
+  redirect '/'
 end
